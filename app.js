753bd5a3b43e0fc71e821a7c38f040e58f8dc287
Abdul-Hannan786 <abdulhannanhere@gmail.com>
2024-07-24 02:33:45 +0500

    working on javascript

---
+++ app.js
@@ -0,0 +1,27 @@
+const container = document.querySelector(".container");
+const search = document.querySelector(".search-box button");
+const weatherBox = document.querySelector(".weather-box");
+const weatherDetails = document.querySelector(".weather-details");
+const error404 = document.querySelector(".not-found");
+
+search.addEventListener("submit", () => {
+  const APIKey = dc75b1f9b6ad7b01eaed401a35035418;
+  const city = document.querySelector(".search-box input").value;
+
+  if (city.trim() === "") return;
+
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+     .then((response) => response.json())
+     .then(json => {
+
+          if(json.cod === '404'){
+            container.style.height = '400px';
+            weatherBox.style.display = 'none';
+            weatherDetails.style.display = 'none';
+            error404.style.display = 'block';
+            error404.classList.add('fadeIn');
+            return;
+          }
+
+     });
+});
